a305bcb741a912e17739b72e820c0e18740d9ea0
kareemHamed5 <136795123+kareemHamed5@users.noreply.github.com>
2024-01-28 06:51:50 +0200

    Add files via upload

---
+++ templet.js
@@ -0,0 +1,113 @@
+
+// // if (document.getElementById('tt').innerHTML === ) {
+// //     clearInterval(u)
+// //     console.log(44444)
+// // }
+
+
+// let ii = document.getElementById('tt')
+
+// // console.log(ip)
+
+// // let o =setInterval(() => {
+// //     ii.innerHTML++
+// // }, 10);
+
+// // if (ii.textContent =='100') {
+// //     console.log(4400)
+// //     clearInterval(o)
+// // }
+// // console.log(ii.te)
+
+
+// // let u = document.getElementById('tt')
+
+// //     let ip = u.dataset.num
+// //     let pppp =setInterval(() => {
+// //         u.textContent++
+// //         let res = false
+
+// // if (res == false) {
+// //     clearInterval(pppp)
+// //     window.onscroll = _ => {
+// //     if (scrollY >= 150) {
+// //         console.log(111);
+// //         res = true;
+// //         if (res = true) {
+// //             console.log(4444)
+// //         }
+// //     }
+// // }
+
+// // }
+// //     if (u.textContent == ip) {
+// //     clearInterval(pppp)
+// // }
+// // }, 10);
+
+
+
+
+
+// // let header = document.getElementById('header')
+// // window.onscroll = () => {
+// //     if (scrollY >= 300) {
+// //         header.style.cssText="background-color:#fff; top: 0;"
+// //     } else {
+// //         header.style.cssText="backgroundColor:red; top: 0;"
+
+// //     }
+// //     if (scrollY >= 600) {
+// //         // document.querySelector('.fa-arrow-down').style.display='flex';
+// //         document.querySelector('.fa-arrow-down').style.opacity= 1;
+// //         document.querySelector('.fa-arrow-down').style.zIndex= 1;
+// //     } else {
+// //         document.querySelector('.fa-arrow-down').style.opacity = 0;
+// //         document.querySelector('.fa-arrow-down').style.zIndex= -77;
+        
+        
+// //     }
+
+// // }
+
+
+// // document.querySelectorAll('nav ul li').forEach((e) => {
+// //     e.onclick = _ => {
+// //         document.querySelector('.active').classList.remove('active')
+// //         e.classList.add('active')
+// //     }
+// // })
+
+
+document.getElementById('bars').onclick = _ => {
+    console.log(44)
+     document.querySelector('.header .container ul').classList.toggle('bars-res')
+ }
+document.querySelector('.fa-x').onclick = _ => {
+    document.querySelector('.header .container ul').classList.remove('bars-res')
+}
+
+
+
+// // document.querySelectorAll('.imgs img').forEach((e) => {
+// //     e.onclick = _=>{
+// //         document.getElementById('img').setAttribute('src',e.getAttribute('src'))
+// //     }
+// // })
+
+
+
+
+// // document.querySelector('.landing .start button').onclick = _ => {
+// //     document.querySelector('.landing .flex img').style.height='100%'
+// // }
+
+
+
+
+// // setInterval(() => {
+// //     let p = Math.round(Math.random() * 255)
+// //     console.log(p)
+// //     document.body.style.backgroundColor=`rgb(${p}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`
+// // }, 1000);
+
